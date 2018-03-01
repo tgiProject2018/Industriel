08a2475bb27c273e870cff4f8b2ddd55f76667b0
Jean Senecal <35342305+jeandota@users.noreply.github.com>
2018-03-01 13:48:52 -0500

    Échéancier
    Commencement de la page échéancier

---
+++ siteWeb/echeancier.php
@@ -0,0 +1,177 @@
+<head>
+
+  <!-- Meta -->
+  <!--===============================================================================================-->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <!--===============================================================================================-->
+
+  <!-- Titre -->
+  <!--===============================================================================================-->
+  <title>Connexion</title>
+  <!--===============================================================================================-->
+
+  <!-- Bootstrap core CSS -->
+
+  <!-- Custom fonts for this template -->
+  <!--===============================================================================================-->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800"
+    rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic"
+    rel="stylesheet" type="text/css">
+  <!--===============================================================================================-->
+  <!-- Custom styles for this template -->
+  <!--===============================================================================================-->
+  <link href="css/business-casual.css" rel="stylesheet">
+  <link rel="icon" href="img/committee.png">
+  <!--===============================================================================================-->
+
+</head>
+
+<body>
+
+  <!-- Titre et adresse -->
+  <!--===============================================================================================-->
+  <div class="tagline-upper text-center text-heading text-shadow text-white mt-5 d-none d-lg-block">Comit&eacute de Loisirs d'Ivaco</div>
+  <div class="tagline-lower text-center text-expanded text-shadow text-uppercase text-white mb-5 d-none d-lg-block">
+    1040 COUNTY RD 17 | L'ORIGNAL, ON K0B 1K0 | 613-675-4671</div>
+  <!--===============================================================================================-->
+
+  <!-- Navigation -->
+  <!--===============================================================================================-->
+  <nav class="navbar navbar-expand-lg navbar-light bg-faded py-lg-4 row">
+    <div class="container col-12">
+      <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Ivaco Loisirs</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item px-lg-2">
+            <a class="nav-link text-uppercase text-expanded" href="index.php">Échéancier
+            </a>
+          </li>
+          <li class="nav-item px-lg-2">
+            <a class="nav-link text-uppercase text-expanded" href="nouvelles.php">ListeClients</a>
+          </li>
+          <li class="nav-item px-lg-2">
+            <a class="nav-link text-uppercase text-expanded" href="achats.php">Historique</a>
+            <span class="sr-only">(current)</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--===============================================================================================-->
+
+  <!-- CSS pour la page -->
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+  <!--===============================================================================================-->
+  <link rel="stylesheet" type="text/css" href="css/util.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
+  <!--===============================================================================================-->
+
+  <!-- Container contenant le login form -->
+  <!--===============================================================================================-->
+  <link href="css/business-casual.css" rel="stylesheet">
+  <div class="limiter">
+    <div class="limiter">
+      <div class="container-login100">
+        <div class="wrap-login100">
+          <form id="login" class="login100-form validate-form" onsubmit="connexion()" method="POST">
+            <span class="login100-form-logo" style="background-image: URL(img/committee3.png)">
+            </span>
+
+            <span class="login100-form-title p-b-34 p-t-27">
+              Connexion
+            </span>
+
+            <div class="wrap-input100 validate-input" data-validate="Enter username">
+              <input id="user" class="input100" type="text" name="username" placeholder="Nom d'utilisateur">
+              <span class="focus-input100" data-placeholder="&#xf207;"></span>
+            </div>
+
+            <div class="wrap-input100 validate-input" data-validate="Enter password">
+              <input id="password" class="input100" type="password" name="password" placeholder="Mot de Passe">
+              <span class="focus-input100" data-placeholder="&#xf191;"></span>
+            </div>
+
+            <div class="container-login100-form-btn">
+              <button class="login100-form-btn">
+                Se connecter
+              </button>
+            </div>
+
+            <div class="text-center p-t-90">
+              <a class="txt1" href="#">
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!--===============================================================================================-->
+
+
+    <!--===============================================================================================-->
+    <div id="dropDownSelect1"></div>
+    <!--===============================================================================================-->
+
+
+    <!-- Javascript -->
+    <!--===============================================================================================-->
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/animsition/js/animsition.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/daterangepicker/moment.min.js"></script>
+    <script src="vendor/daterangepicker/daterangepicker.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <!--===============================================================================================-->
+    <script src="js/main.js"></script>
+    <!--===============================================================================================-->
+
+    <!-- Footer -->
+    <!--===============================================================================================-->
+    <footer class="bg-faded text-center py-5">
+      <div class="container">
+        <p class="m-0">Copyright &copy; Ivaco Loisirs 2017</p>
+      </div>
+    </footer>
+    <!--===============================================================================================-->
+
+    <!-- Bootstrap core JavaScript -->
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+
+</body>
+
+</html>
