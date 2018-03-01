a833db64e947bc10e4c75786151bf53e45336693
CedricLeveille <36856990+CedricLeveille@users.noreply.github.com>
2018-03-01 13:18:01 -0500

    Test
    test

--- siteWeb/echeancier.php
+++ siteWeb/echeancier.php
@@ -1,1 +1,14 @@
-
+condition -> condition & condition
+			|condition & condition
+			|!condition
+			|(condition)
+			|condition < condition
+			|condition <= condition
+			|condition > condition
+			|condition >= condition
+			|condition = condition
+			|condition != condition
+			|INT
+			
+			2<3 & (4!=5) | !4=4
+		
