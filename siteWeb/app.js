8fd7234eb41145c69c013c1b0f1f9d51d0adf455
CedricLeveille <36856990+CedricLeveille@users.noreply.github.com>
2018-03-05 12:25:21 -0500

    NodeJS
    Implémentation de NodeJS pour le serveur.

---
+++ siteWeb/app.js
@@ -0,0 +1,132 @@
+var app = require('express')(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server),
+    ent = require('ent'), // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
+    fs = require('fs');
+    
+// Chargement de la page index.html
+// ============================================================================================================
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+// ============================================================================================================
+
+// Chargement de la page consoleetudiant.html
+// ============================================================================================================
+app.get('/consoleEtudiant', function (req, res) {
+  res.sendfile(__dirname + '/consoleEtudiant.html');
+});
+// ============================================================================================================
+
+// Chargement de la page consoleprofesseur.html
+// ============================================================================================================
+app.get('/consoleProfesseur', function (req, res) {
+  res.sendfile(__dirname + '/consoleProfesseur.html');
+});
+// ============================================================================================================
+
+// Chargement de la page login.html
+// ============================================================================================================
+app.get('/login', function (req, res) {
+  res.sendfile(__dirname + '/login.html');
+});
+// ============================================================================================================
+
+// On recupere mon image de background
+// ============================================================================================================
+app.get('/school', function (req, res) {
+    res.sendfile(__dirname + '/img/school3.jpeg');
+  });
+// ============================================================================================================
+io.sockets.on('connection', function (socket) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    // ============================================================================================================
+    socket.on('nouveau_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+    // ============================================================================================================
+
+    // Dès qu'on nous donne un professeur, (username password), on le stocke en variable de session et on informe
+    // les autres utilisateurs
+    // ============================================================================================================
+    socket.on('nouveau_professeur', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_professeur', pseudo);
+    });
+    // ============================================================================================================
+
+    // On ecoute le login_formulaire pour authentifier une personne lorsqu'il entre ses données
+    // ============================================================================================================
+    socket.on('login_formulaire', function(username,password) {
+
+        // On se connecte a notre base de données
+        // --------------------------------------------------------------------------------------------------------
+        var mysql = require('mysql');
+        var con = mysql.createConnection({
+            host: "localhost", user: "root",
+            password: "",
+            database: "projetnodejs"
+        });
+        // --------------------------------------------------------------------------------------------------------
+          
+        // S'il a une erreur à la connection (par exemple XAMPP n'est pas running)
+        // --------------------------------------------------------------------------------------------------------
+        con.connect(function(queryErreur) {
+            if (queryErreur) {
+                throw queryErreur;
+            }
+        // --------------------------------------------------------------------------------------------------------
+
+            // On fait un select dans notre database dans notre table user pour voir si le user existe et procède aux étapes de vérification
+            // ------------------------------------------------------------------------------------------------------------------------------
+            con.query("SELECT Username, Password FROM user WHERE user.Username = '" + username + "' and user.Password = '" + password + "'", 
+            function (queryErreur, queryRetour) {
+
+                // Si on trouve une erreur dans la query, on throw
+                // **************************************************************************************************************************
+                if(queryErreur) {
+                    throw queryErreur;
+                }
+                // **************************************************************************************************************************
+
+                // Si on a rien, on emit notre fonction qui alertera notre utilisateur que quelque chose ne marche pas
+                // **************************************************************************************************************************
+                if(!queryRetour[0])
+                {
+                    socket.emit('unauthorized_login'); 
+                    return false;
+                }
+                // **************************************************************************************************************************
+
+                // Si les données sont bonnes, on redirige l'utilisateur à la page professeur
+                // **************************************************************************************************************************
+                if( password == queryRetour[0].Password && username == queryRetour[0].Username) {
+                    socket.emit('redirect', "consoleProfesseur", queryRetour[0].Username);
+                }
+                // **************************************************************************************************************************
+
+                // Si les données sont mauvaises, on alerte l'utilisateur.
+                // **************************************************************************************************************************
+                else {
+                    socket.emit('unauthorized_login');
+                }
+                // **************************************************************************************************************************
+            });
+            // ------------------------------------------------------------------------------------------------------------------------------
+        });
+    });
+    // ============================================================================================================
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    // ============================================================================================================
+    socket.on('nouveau_message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('nouveau_message', {pseudo: socket.pseudo, message: message});
+    }); 
+    // ============================================================================================================
+});
+
+server.listen(8080);
