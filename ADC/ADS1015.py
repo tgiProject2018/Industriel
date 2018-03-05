17c5e7f1f4374296eec68765e4ff01749ab9f2ae
BrunetTristan97 <brunet.tristan97@gmail.com>
2018-03-05 12:49:46 -0500

    Threads, Driver mis à jour
    Définit les threads et continué le développement du driver

--- ADC/ADS1015.py
+++ ADC/ADS1015.py
@@ -1,3 +1,25 @@
+ADS1015_DEFAULT_ADDRESS = 0x0048
+ADS1015_CONVERSION_POINTER = 0x00
+ADS1015_CONFIG_POINTER = 0x01
+ADS1015_LOW_THRESH_POINTER = 0x02
+ADS1015_HIGH_THRESH_POINTER = 0x03
+
 class ADS1015(object):
-    def __init__:
-        
+    def __init__(self, address=ADS1015_DEFAULT_ADDRESS, i2c=None, **kwargs):
+        if i2c is None:
+            import Adafruit_GPIO.I2C as I2C
+            i2c = I2C
+        self._device = i2c.get_i2c_device(address, **kwargs)
+        self.configure()
+
+    def read_level():
+        self._device.writeList()
+        # Read Config register OS bit to check if we are currently 
+        # performing a conversion
+        #level = _device.
+
+    def configure():
+        self.data_rate = 0x0080 # 1600
+        self.mux = 0 # Config par defaut
+        self.mode = 1 # Power-down single-shot mode
+
--- ADC/main.py
+++ ADC/main.py
@@ -1,1 +1,43 @@
 import Adafruit_ADS1015
+import threading
+
+
+
+class Tache_ReadADS1015(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        ads1015 = ADS1015()
+        while True:
+            ads1015.read_level()
+
+
+class Tache_ReadTemp(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        therm = Temp()
+        while True:
+            therm.read_temp()
+
+class Tache_Shell(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        IndustrielShell().cmdloop()
+
+##main
+#tacheADS1015 = Tache_ReadADS1015(1, "tache_ADS1015", 1)
+tacheTemperature = Tache_ReadTemp(2, "tache_Temperature", 2)
+tacheShell = Tache_Shell(1, "tache_Shell", 1)
+
+#tacheADS1015.start()
+tacheTemperature.start()
+tacheShell.start()
