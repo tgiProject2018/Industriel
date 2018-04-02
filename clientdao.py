3a727a711a22c320da72c562041d7a414d066254
Pakkal <dglanthier@hotmail.com>
2018-04-02 15:36:03 +0000

    Fix les string en parametre

--- clientdao.py
+++ clientdao.py
@@ -1,13 +1,12 @@
-from saveUser import *
-import sqlite3 import Error
-import mysql.connector
+#from saveUser import *
+import sqlite3 
 #Couche dacces de donne vers le bl
 class ClientDao() :
 	#Chemin de la base de donne local
 	#A modifier
 	path = ''
 	def create_connection() :
-		db_file=path
+		db_file = path
 		try: 
 			conn = sqlite3.connect(db_file)
 			return conn
@@ -16,24 +15,24 @@ class ClientDao() :
 		return none
 	
 	#Sauvegarde un client dans la base de donne local
-	def saveUserLocal(*args) :
+	def saveUserLocal(user) :
 		conn = create_connection()
 		with conn:
 			cur = conn.cursor()
-			cur.execute("INSERT INTO tblclient(prenom,nom) values (%s)")
+			cur.execute("INSERT INTO tblclient(prenom,nom) values (%s,%s)" , user.prenom,user.name)
 		cur.close()
 		conn.commit()
 		conn.close()
 	
 	#Sauvegarde la temperature et le niveau dans le melange
-	def saveMelange(*args):
+	def saveMelange(melange):
 		conn = create_connection()
 		with conn:
 			cur = conn.cursor()
-			cur.execute("INSERT INTO tblmelange(temperature,quantite,niveau,date) values (%s,datetime.now())")
-			cur.execute("INSERT INTO tblassoClientMelange(idClient,idMelange) values (%s,last_insert_rowid())")
+			cur.execute("INSERT INTO tblmelange(temperature,quantite,niveau,date) values (%f,%f,%d,datetime.now())" , melange.temperature,melange.quantite,melange.niveau)
+			#cur.execute("INSERT INTO tblassoClientMelange(idClient,idMelange) values (%s,last_insert_rowid())")
 		cur.close()
 		conn.commit()
-		conn.close()
+conn.close()
 	
 	
