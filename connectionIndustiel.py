c0fe9f226051e24031db2ee642eb5c5694165835
PaulTadi <36857177+PaulTadi@users.noreply.github.com>
2018-04-02 12:16:12 -0400

    connection bd avec industriel

---
+++ connectionIndustiel.py
@@ -0,0 +1,67 @@
+from socketIO_client import SocketIO
+import mysql.connector
+
+
+## Connection a la base de donnee
+cnx = mysql.connector.connect(user='root', password='',
+	host='127.0.0.1',
+	database='dbsystemcontrolleur')
+cursor = cnx.cursor()
+
+## Deleguees
+def on_connect():
+	print('connect')
+def on_disconnect():
+	print('disconnect')
+def on_reconnect():
+	print('reconnect')
+	
+def afficherToutLesClient(*args):
+	#idPatient = args[0]
+	query = ("Select id, prenom, nom, adresse from tblClient");
+	cursor.execute(query);
+	#Select x.id, prenom, nom, y.date from tbpatient x
+	#inner join tbpriseteste y
+	#on(y.idClient = x.id)
+	#Where y.id = (select max(id) from tbpriseteste where idClient = x.id);
+	results = cursor.fetchall();
+	socketIO.emit('afficherClients', results);
+	cursor.close();
+	cnx.close();
+def modifierClient(*args):
+	idClient = args[0];
+	query = ("update tblClient set prenom = %s, nom = %s, adresse = %s where id = %s");
+	cursor.execute(query, (args[1],args[2],args[3],idClient));
+	cursor.close();
+	cnx.close();
+	
+def afficherMelanges(*args):
+	idClient = args[0];
+	query = ("Select dateProduction, niveau, temperature from tblMelange where idClient = %s");
+	cursor.execute(query,idClient);
+	results = cursor.fetchall();
+	socketIO.emit('afficherMelanges',results);
+	cursor.close();
+	cnx.close();
+
+def authentification(*args):
+	user = args[0];
+	password = args[1];
+	query = ("Select id from tblEmployer where user = %s and password = %s");
+	cursor.execute(query,(user,password));
+	if cursor.rowcount == 1
+		socketIO.emit('authentification',true);
+	else
+		socketIO.emit('authentification',false);
+	
+	
+## 'main'
+#socketIO.on('afficherInfo', afficherInfo)
+socketIO.on('afficherClients', afficherToutLesClient)
+socketIO.on('modifierClient', modifierClient)
+socketIO.on('afficherMelanges', afficherMelanges)
+## Loop principal
+socketIO.wait()
+## On fait le menage
+cursor.close()
+cnx.close()
